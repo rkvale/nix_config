aa68216088b5f9368fe721d1b2e404b8bbfe67dc
rkvale <rune@kvale.io>
2024-08-05 13:55:13 +0200

    NIX - keyboard no

--- home/home.nix
+++ home/home.nix
@@ -7,7 +7,7 @@
     ./programs/neovim.nix
     ./programs/alacritty.nix
     ./programs/river.nix
-    ./programs/kitty.nix
+    #    ./programs/kitty.nix
   ];
 
   # Home Manager needs a bit of information about you and the
@@ -26,6 +26,8 @@
   };
 
 # programs.fish.shellAliases.nrs = "nh os switch ~/Documents/nix_config";
+
+  home.keyboard.layout = "no";
 
   #hyprlock config file
   home.file.".config/hypr/hyprlock.conf" = {
