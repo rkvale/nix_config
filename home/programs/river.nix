d638aac21581e2e3b961ff54419890c97d5eaaaf
rkvale <rune@kvale.io>
2024-08-09 07:18:02 +0200

    nix river rule-add

--- home/programs/river.nix
+++ home/programs/river.nix
@@ -155,6 +155,7 @@
             "ssd"
             "-app-id WebCord tags ${builtins.elemAt tagMasks 9}"
             "-app-id steam tags ${builtins.elemAt tagMasks 8}"
+            "-app-id firefox tags ${builtins.elemAt tagMasks 7}"
           ];
         }
 
