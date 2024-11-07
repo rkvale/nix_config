b5d7396bee174e84e2a94c7a3c760c7c29db199f
Rune Kvale <rune@kvale.io>
2024-11-07 11:35:35 +0100

    shortcut for nvim

--- home/programs/river.nix
+++ home/programs/river.nix
@@ -171,6 +171,7 @@
             # subject to punishment by death penalty + life in prison, in that order.
             "Super Return" = "kitty --single-instance --instance-group river";
             "Super P" = "riverctl spawn \"$(tofi-drun)\"";
+            "Super N" = "kitty -e nvim"
 
             # Same as Hyprland, this is temporary until I find something else.
             #"Super+Shift L" = "hyprlock";
