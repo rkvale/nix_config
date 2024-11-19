8ac7398d5e01f0d4e2ada75faaa1700948edaf05
Rune Kvale <rune@kvale.io>
2024-11-19 09:59:41 +0100

    tailscale

--- configuration.nix
+++ configuration.nix
@@ -147,6 +147,10 @@
 #    "1password"
 #  ];
 
+ #Tailscale 
+  services.tailscale.enable = true;
+
+
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
