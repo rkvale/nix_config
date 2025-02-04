674afa0a3630064dccf1effbbaff838443d30e69
Rune Kvale <rune@kvale.io>
2025-02-04 08:50:17 +0100

    Helix shortcut

--- home/programs/river.nix
+++ home/programs/river.nix
@@ -170,7 +170,7 @@
             # subject to punishment by death penalty + life in prison, in that order.
             "Super Return" = "kitty --single-instance --instance-group river";
             "Super P" = "riverctl spawn \"$(tofi-drun)\"";
-            "Super N" = "kitty --single-instance --instance-group river -e hx";
+            "Super E" = "kitty --single-instance --instance-group river -e hx";
 
             # Same as Hyprland, this is temporary until I find something else.
             #"Super+Shift L" = "hyprlock";
