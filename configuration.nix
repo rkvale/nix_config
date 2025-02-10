8ac6642b0f47538e648b8de6b3180464cf0941aa
Rune Kvale <rune@kvale.io>
2025-02-10 12:17:12 +0100

    language server vscode

--- configuration.nix
+++ configuration.nix
@@ -211,6 +211,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    vscode-langservers-extracted
     jaq
     swappy
     #helix
