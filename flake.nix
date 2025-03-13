723fda98cbee9453e8d9e2f14223485b0bb53577
Rune Kvale <rune@kvale.io>
2025-03-13 22:53:32 +0100

    noe med flake, husker ikke hva og burde sikkert gjort commit med en gang :-)

--- flake.nix
+++ flake.nix
@@ -5,8 +5,9 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    sops-nix.url = "github:Mic92/sops-nix"; # secrets
 
+    # Sops
+    sops-nix.url = "github:Mic92/sops-nix"; # secrets
     sops-nix.inputs.nixpkgs.follows = "nixpkgs";
 
 
