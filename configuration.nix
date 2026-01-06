e9b1a2c814f0292d9433d411800f62da461f0e46
Rune Kvale <rune@kvale.io>
2026-01-06 08:39:39 +0100

    add entry to host file

--- configuration.nix
+++ configuration.nix
@@ -85,6 +85,7 @@
     hosts = {
       "192.168.1.30" = ["fibaro.kvale.io"];
       "192.168.1.1" = ["router.kvale.io"];
+      "192.168.1.20" = ["ha.kvale.io"];
     };
 
     nftables.enable = true;
