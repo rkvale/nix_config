6a7c2b46f129eddd13b34e1494594170d7f01bb2
Rune Kvale <rune@kvale.io>
2026-01-08 22:57:25 +0100

    add newha to hosts

--- configuration.nix
+++ configuration.nix
@@ -86,6 +86,7 @@
       "192.168.1.30" = ["fibaro.kvale.io"];
       "192.168.1.1" = ["router.kvale.io"];
       "192.168.1.20" = ["ha.kvale.io"];
+      "192.168.1.3" = ["newha.kvale.io"];
     };
 
     nftables.enable = true;
