9c85442b6639e9ef2195546ed0adec1bf38ac1bb
Rune Kvale <rune@kvale.io>
2026-08-07 08:50:41 +0200

    swww to awww

--- home/programs/swww.nix
+++ home/programs/swww.nix
@@ -1,20 +1,20 @@
 { lib, config, ... }:
 let
   inherit (lib) mkOption types;
-  cfg = config.services.swww;
+  cfg = config.services.awww;
   setBackground = "${lib.getExe cfg.package} img -t none ${cfg.defaultImage}";
 in
 {
-  options.services.swww = {
+  options.services.awww = {
     defaultImage = mkOption {
       type = types.nullOr types.path;
       default = null;
       example = lib.literalExpression ''"./wallpaper.png"'';
-      description = "A default wallpaper to apply using `swww img` after starting the daemon.";
+      description = "A default wallpaper to apply using `awww img` after starting the daemon.";
     };
   };
   config = lib.mkIf cfg.enable {
-    systemd.user.services.swww = {
+    systemd.user.services.awww = {
       Service = {
         Type = "notify";
         ExecStartPost = lib.mkIf (cfg.defaultImage != null) setBackground;
